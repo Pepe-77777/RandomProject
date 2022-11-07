3e22741de75091944114d131c4c4d1cf6be58482
Pepê <81209437+Pepe-77777@users.noreply.github.com>
2022-11-07 15:55:02 -0300

    Update Updated.bat

--- Updated.bat
+++ Updated.bat
@@ -4,7 +4,6 @@ chcp 65001 > nul
 echo Olá.
 echo.
 echo Este é o projeto  A l e a t ó r i o .
-powershell -Command "ttpInvoke-WebRequest hs://raw.githubusercontent.com/Pepe-77777/RandomProject/main/list.txt -OutFile list.txt"
 type list.txt
 del list.txt > nul
 pause > nul
